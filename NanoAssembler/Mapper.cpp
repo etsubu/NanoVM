e9b169f0358424788d5da0d301975f8928ad6259
etsubu <etsubu@gmail.com>
2018-10-07 17:03:21 +0300

    Labels can now be resolved if all instructions between have already been assembled. See example: labels.nano Assembler now adds "halt" instruction at the end of generated bytecode if one wasn't added in the .nano code

--- NanoAssembler/Mapper.cpp
+++ NanoAssembler/Mapper.cpp
@@ -58,22 +58,111 @@ Mapper::~Mapper() {
 
 }
 
-int64_t Mapper::mapLabel(std::string label, unsigned int instructionIndex, std::unordered_map<std::string, unsigned int> labelMap, std::vector<std::vector<unsigned char>> bytecode) {
+bool Mapper::canMapLabel(std::string label, unsigned int instructionIndex, std::unordered_map<std::string, unsigned int> labelMap, std::vector<Instruction> instructions) {
 	unsigned int labelIndex;
 	try {
-		labelIndex = labelMap[label];
+		labelIndex = labelMap.at(label);
+	}
+	catch (std::out_of_range) {
+		return -1;
+	}
+	if (labelIndex == instructionIndex) {
+		return 1;
+	}
+	if (labelIndex > instructionIndex) {
+		for (int i = instructionIndex + 1; i < labelIndex; i++) {
+			if (!instructions[i].length) {
+				return false;
+			}
+		}
+	}
+	else {
+		for (unsigned int i = instructionIndex - 1; i >= labelIndex; i--) {
+			if (!instructions[i].length) {
+				return false;
+			}
+			if (i == 0)
+				return true;
+		}
+	}
+	return true;
+}
+
+int Mapper::calculateSizeRequirement(std::string label, unsigned int instructionIndex, std::unordered_map<std::string, unsigned int> labelMap, std::vector<Instruction> instructions) {
+	unsigned int labelIndex;
+	try {
+		labelIndex = labelMap.at(label);
 	}
 	catch (std::out_of_range) {
 		return 0;
 	}
 	if (labelIndex == instructionIndex) {
-		// This will cause infinite loop
 		return 0;
 	}
+	int64_t delta;
 	if (labelIndex > instructionIndex) {
-		int64_t delta = sizeof(uint64_t) + 2; // assume max length for the jump instruction (10 bytes)
+		delta = sizeof(uint64_t) + 2; // assume max length for the jump instruction (10 bytes)
 		for (unsigned int i = instructionIndex + 1; i < labelIndex; i++) {
-			size_t instructionLength = bytecode[i].size();
+			size_t instructionLength = instructions[i].length;
+			int unAssembled = 0;
+			if (instructionLength == 0) {
+				// Instruction hasn't been assembled yet because it contains label that hasn't been resolved, thus it's length is unknown
+				unAssembled++;
+			}
+			else {
+				delta += instructionLength;
+			}
+			if (unAssembled) {
+				// assume the unassembled instructions will take max space
+				delta += (unAssembled * (2 + sizeof(uint64_t)));
+			}
+		}
+	}
+	else {
+		delta = 0;
+		for (unsigned int i = instructionIndex - 1; i >= labelIndex; i--) {
+			size_t instructionLength = instructions[i].length;
+			int unAssembled = 0;
+			if (instructionLength == 0) {
+				// Instruction hasn't been assembled yet because it contains label that hasn't been resolved, thus it's length is unknown
+				unAssembled++;
+			}
+			else {
+				delta += instructionLength;
+			}
+			if (unAssembled) {
+				// assume the unassembled instructions will take max space
+				delta += (unAssembled * (2 + sizeof(uint64_t)));
+			}
+			if (i == 0)
+				break;
+		}
+	}
+	if (delta <= SCHAR_MAX -1)
+		return sizeof(int8_t);
+	else if (delta <= SHRT_MAX - 1)
+		return sizeof(int16_t);
+	else if (delta <= INT32_MAX - 1)
+		return sizeof(int32_t);
+	return sizeof(int64_t);
+}
+
+unsigned int Mapper::mapLabel(std::string label, unsigned int instructionIndex, std::unordered_map<std::string, unsigned int> labelMap, std::vector<Instruction> &instructions, int64_t &value) {
+	unsigned int labelIndex;
+	try {
+		labelIndex = labelMap.at(label);
+	}
+	catch (std::out_of_range) {
+		return 0;
+	}
+	if (labelIndex == instructionIndex) {
+		return 0;
+	}
+	int64_t delta;
+	if (labelIndex > instructionIndex) {
+		delta = sizeof(uint64_t) + 2; // assume max length for the jump instruction (10 bytes)
+		for (unsigned int i = instructionIndex + 1; i < labelIndex; i++) {
+			size_t instructionLength = instructions[i].length;
 			int unAssembled = 0;
 			if (instructionLength == 0) {
 				// Instruction hasn't been assembled yet because it contains label that hasn't been resolved, thus it's length is unknown
@@ -84,15 +173,43 @@ int64_t Mapper::mapLabel(std::string label, unsigned int instructionIndex, std::
 				delta += instructionLength;
 			}
 			if (unAssembled) {
-				// Calculate min and max values for delta
-				size_t min = delta + (unAssembled * 2);
-				size_t max = delta + (unAssembled * (2 + sizeof(uint64_t)));
-				if (min <= SCHAR_MAX && max <= SCHAR_MAX) {
-					// 
-				}
+				// assume the unassembled instructions will take max space
+				delta += (unAssembled * (2 + sizeof(uint64_t)));
 			}
 		}
 	}
+	else {
+		delta = 0; // assume max length for the jump instruction (10 bytes)
+		for (unsigned int i = instructionIndex - 1; i >= labelIndex; i--) {
+			size_t instructionLength = instructions[i].length;
+			int unAssembled = 0;
+			if (instructionLength == 0) {
+				// Instruction hasn't been assembled yet because it contains label that hasn't been resolved, thus it's length is unknown
+				unAssembled++;
+				return 0;
+			}
+			else {
+				delta += instructionLength;
+			}
+			if (unAssembled) {
+				// assume the unassembled instructions will take max space
+				delta += (unAssembled * (2 + sizeof(uint64_t)));
+			}
+			if (i == 0)
+				break;
+		}
+		delta = -delta;
+	}
+	value = delta;
+	if (instructions[instructionIndex].length)
+		return instructions[instructionIndex].length - 2;
+	if (SCHAR_MIN <= value && value <= SCHAR_MAX)
+		return sizeof(int8_t);
+	else if (SHRT_MIN <= value && value <= SHRT_MAX - 1)
+		return sizeof(int16_t);
+	else if (INT32_MIN <= value && value <= INT32_MAX - 1)
+		return sizeof(int32_t);
+	return sizeof(int64_t);
 }
 
 bool Mapper::mapRegister(std::string regName, unsigned char& reg) {
@@ -117,6 +234,48 @@ bool Mapper::mapOpcode(std::string opcodeName, Instruction &instruction) {
 	}
 }
 
+template<typename T> void Mapper::mapImmediate(unsigned char *bytes, T value) {
+	for (unsigned int i = 0; i < sizeof(T); i++) {
+		bytes[i] = static_cast<uint8_t>(value >> ((sizeof(T) * 8) - 8));
+	}
+}
+
+int Mapper::mapInteger(int64_t value64, unsigned char* bytes, unsigned int &length) {
+	if (length == sizeof(int8_t) || (!length && INT8_MIN <= value64 && value64 <= INT8_MAX)) {
+		bytes[0] = static_cast<int8_t>(value64);
+		length = sizeof(int8_t);
+		return Byte;
+	}
+	else if (length == sizeof(int16_t) || (INT16_MIN <= value64 && value64 <= INT16_MAX)) {
+		uint16_t value16 = static_cast<int16_t>(value64);
+		bytes[0] = (static_cast<int8_t>(value16 >> 8));
+		bytes[1] = (static_cast<int8_t>(value16));
+		length = sizeof(int16_t);
+		return Short;
+	}
+	else if (length == sizeof(int32_t) || (INT32_MIN <= value64 && value64 <= INT32_MAX)) {
+		uint32_t value32 = static_cast<int32_t>(value64);
+		bytes[0] = (static_cast<int8_t>(value32 >> 24));
+		bytes[1] = (static_cast<int8_t>(value32 >> 16));
+		bytes[2] = (static_cast<int8_t>(value32 >> 8));
+		bytes[3] = (static_cast<int8_t>(value32));
+		length = sizeof(int32_t);
+		return Dword;
+	}
+	else {
+		bytes[0] = (static_cast<int8_t>(value64 >> 56));
+		bytes[1] = (static_cast<int8_t>(value64 >> 48));
+		bytes[2] = (static_cast<int8_t>(value64 >> 40));
+		bytes[3] = (static_cast<int8_t>(value64 >> 32));
+		bytes[4] = (static_cast<int8_t>(value64 >> 24));
+		bytes[5] = (static_cast<int8_t>(value64 >> 16));
+		bytes[6] = (static_cast<int8_t>(value64 >> 8));
+		bytes[7] = (static_cast<int8_t>(value64));
+		length = sizeof(int64_t);
+		return Qword;
+	}
+}
+
 int Mapper::mapImmediate(std::string value, unsigned char* bytes, unsigned int &length) {
 	if (value.empty())
 		return -1;
@@ -124,39 +283,7 @@ int Mapper::mapImmediate(std::string value, unsigned char* bytes, unsigned int &
 		if (value[0] == '-')
 		{
 			int64_t value64 = std::stoll(value);
-			if (value64 <= INT8_MAX) {
-				bytes[0] = static_cast<int8_t>(value64);
-				length = sizeof(int8_t);
-				return Byte;
-			}
-			else if (value64 <= INT16_MAX) {
-				uint16_t value16 = static_cast<int16_t>(value64);
-				bytes[0] = (static_cast<int8_t>(value16 >> 8));
-				bytes[1] = (static_cast<int8_t>(value16));
-				length = sizeof(int16_t);
-				return Short;
-			}
-			else if (value64 <= INT32_MAX) {
-				uint32_t value32 = static_cast<int32_t>(value64);
-				bytes[0] = (static_cast<int8_t>(value32 >> 24));
-				bytes[1] = (static_cast<int8_t>(value32 >> 16));
-				bytes[2] = (static_cast<int8_t>(value32 >> 8));
-				bytes[3] = (static_cast<int8_t>(value32));
-				length = sizeof(int32_t);
-				return Dword;
-			}
-			else {
-				bytes[0] = (static_cast<int8_t>(value64 >> 56));
-				bytes[1] = (static_cast<int8_t>(value64 >> 48));
-				bytes[2] = (static_cast<int8_t>(value64 >> 40));
-				bytes[3] = (static_cast<int8_t>(value64 >> 32));
-				bytes[4] = (static_cast<int8_t>(value64 >> 24));
-				bytes[5] = (static_cast<int8_t>(value64 >> 16));
-				bytes[6] = (static_cast<int8_t>(value64 >> 8));
-				bytes[7] = (static_cast<int8_t>(value64));
-				length = sizeof(int64_t);
-				return Qword;
-			}
+			return mapInteger(value64, bytes, length);
 		}
 		else {
 			uint64_t value64 = std::stoull(value);
--- NanoAssembler/Mapper.h
+++ NanoAssembler/Mapper.h
@@ -12,7 +12,12 @@ public:
 	bool mapOpcode(std::string, Instruction&);
 	bool mapRegister(std::string, unsigned char&);
 	int mapImmediate(std::string, unsigned char*, unsigned int&);
-	int64_t mapLabel(std::string label, unsigned int instructionIndex, std::unordered_map<std::string, unsigned int> labelMap, std::vector<std::vector<unsigned char>> bytecode);
+	bool canMapLabel(std::string label, unsigned int instructionIndex, std::unordered_map<std::string, unsigned int> labelMap, std::vector<Instruction> instructions);
+	int calculateSizeRequirement(std::string label, unsigned int instructionIndex, std::unordered_map<std::string, unsigned int> labelMap, std::vector<Instruction> instructions);
+	unsigned int mapLabel(std::string label, unsigned int instructionIndex, std::unordered_map<std::string, unsigned int> labelMap, std::vector<Instruction> &instructions, int64_t &value);
+	int mapInteger(int64_t value64, unsigned char* bytes, unsigned int &length);
+	template<typename T> void mapImmediate(unsigned char *bytes, T value);
+
 private:
 	std::unordered_map<std::string, std::pair<unsigned char, unsigned int>> opcodeMap;
 	std::unordered_map<std::string, unsigned char> registerMap;
